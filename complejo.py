de8905a3a6703635326f5fec8db83e1e3574a4e6
Mauricio González <46940813+lmgonzalezc@users.noreply.github.com>
2019-03-28 08:00:58 -0500

    Intento complejo

--- complejo.py
+++ complejo.py
@@ -1,17 +1,15 @@
 import numpy as np
 import unittest
-import complejo
 import math
-
 class Complejo():
     def __init__ (self, Imag, Real):
         self.imaginario=Imag
         self.real=Real
         self.norma=np.sqrt(Imag**2+Real**2)
     def conjugado(self):
-        return (self.real-self.imaginario)
+        self.imaginario=-self.imaginario
     def calcula_norma(self):
-        return (np.sqrt(Imag**2+Real**2))
+        self.norma=np.sqrt(Imag**2+Real**2))
     def pow(self, power):
         return ((self.real+self.imaginario)**power)
         
