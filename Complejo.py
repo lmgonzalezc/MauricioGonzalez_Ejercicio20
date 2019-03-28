a5d01c812aea62df6a2c7de0a6ed1e65a424c961
Mauricio González <46940813+lmgonzalezc@users.noreply.github.com>
2019-03-28 07:44:25 -0500

    Add files via upload

---
+++ Complejo.py
@@ -0,0 +1,16 @@
+import numpy as np
+import unittest
+import complejo
+import math
+class Complejo():
+    def __init__ (self, Imag, Real):
+        self.imaginario=Imag
+        self.real=Real
+        self.norma=np.sqrt(Imag**2+Real**2)
+    def conjugado(self):
+        return (self.real-self.imaginario)
+    def calcula_norma(self):
+        return (np.sqrt(Imag**2+Real**2))
+    def pow(self, power):
+        return ((self.real+self.imaginario)**power)
+        
